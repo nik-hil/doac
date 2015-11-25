2ba2e13d4202540ce0fa7f178fe333b2e5fed59c
Nikhileshkumar Ikhar <nikhil.ikhar@gmail.com>
2015-11-25 14:57:12 +0530

    changed user model import
    This is required in django 1.7

--- doac/compat.py
+++ doac/compat.py
@@ -20,9 +20,9 @@ def get_user_model():
     """
 
     try:
-        from django.contrib.auth import get_user_model
+        from django.conf import settings
         
-        return get_user_model()
+        return settings.AUTH_USER_MODEL
     except ImportError:
         from django.contrib.auth.models import User
         
